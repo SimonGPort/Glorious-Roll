5ccc52642badc718c6b4e6169866bcc9102e8f62
SimonGPort <simon.giroux.portelance@gmail.com>
2020-04-29 14:24:34 -0400

    Project

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,10 @@ import { Provider } from "react-redux";
 import store from "./store.js";
 
 import reloadMagic from "./reload-magic-client.js"; // automatic reload
-reloadMagic(); // automatic reload
+
+if (process.env.NODE_ENV === "development") {
+  reloadMagic(); // automatic reload
+}
 
 ReactDOM.render(
   <Provider store={store}>
